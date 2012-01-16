26e9c332583c080a72690ff899c81c8f0d64a1ab
Igor Sobreira <igor@igorsobreira.com>
2012-01-16 13:42:52 -0200

    Create in-memory persistense layer

---
+++ oauth2u/server/database.py
@@ -0,0 +1,9 @@
+AUTHORIZATIONS = {}
+
+
+def new_authorization(code, client_id, redirect_uri):
+    AUTHORIZATIONS[code] = {'client_id': client_id, 'redirect_uri': redirect_uri}
+
+
+def find_authorization(code):
+    return AUTHORIZATIONS.get(code)
---
+++ tests/server/test_database.py
@@ -0,0 +1,18 @@
+from oauth2u.server import database
+
+
+def test_find_should_return_none_if_no_authorization_is_created():
+    auth = database.find_authorization(code='1234')
+
+    assert auth is None
+
+
+def test_should_be_able_to_save_and_retrieve_authorizations():
+    database.new_authorization(code='1234', client_id='client1',
+                               redirect_uri='http://callback/return')
+
+    auth = database.find_authorization(code='1234')
+
+    assert 'client1' == auth['client_id']
+    assert 'http://callback/return' == auth['redirect_uri']
+
