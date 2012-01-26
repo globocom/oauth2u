f8d07a2a3ff12e1b3c7e95d5220292a2e06b867c
Igor Sobreira <igor@igorsobreira.com>
2012-01-26 09:29:54 -0200

    Add default parameters do logging configuration

--- oauth2u/server/log.py
+++ oauth2u/server/log.py
@@ -1,7 +1,15 @@
 import logging
 
+DEFAULT_LEVEL = logging.INFO
+DEFAULT_FORMAT = '%(asctime)s:%(levelname)s:%(name)s: %(message)s'
+
 def configure(**kwargs):
-    logging.basicConfig(**kwargs)
+    defaults = {
+        'level': DEFAULT_LEVEL,
+        'format': DEFAULT_FORMAT,
+        }
+    defaults.update(kwargs)
+    logging.basicConfig(**defaults)
 
 
 def critical(msg, *args, **kwargs):
--- tests/server/test_log.py
+++ tests/server/test_log.py
@@ -3,14 +3,16 @@ import mock
 from oauth2u.server import log
 
 
-def test_configure_should_call_basicConfig_on_logging(monkeypatch):
+def test_configure_should_call_basicConfig_on_logging_with_default_parameters(monkeypatch):
     logging_mock = mock.Mock()
     monkeypatch.setattr(log, 'logging', logging_mock)
 
     log.configure(filename='/tmp/log')
 
     assert 1 == logging_mock.basicConfig.call_count
-    logging_mock.basicConfig.assert_called_with(filename='/tmp/log')
+    logging_mock.basicConfig.assert_called_with(filename='/tmp/log',
+                                                level=log.DEFAULT_LEVEL,
+                                                format=log.DEFAULT_FORMAT)
 
 
 def test_log_functions_are_passed_to_logging_module(monkeypatch):
